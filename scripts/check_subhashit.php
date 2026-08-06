ccf925cbdc4345e9fd492dba6197b84d3b4bd3db
Mayurrrrrrrrrr <mayurdubey2003@gmail.com>
2026-08-06 19:06:20 +0530

    Update check script

--- scripts/check_subhashit.php
+++ scripts/check_subhashit.php
@@ -1,5 +1,5 @@
 <?php
 require_once __DIR__ . '/../config/db.php';
-require_once __DIR__ . '/../includes/PanchangHelper.php';
-$data = PanchangHelper::getForDate($pdo, '2026-08-06', 1);
-print_r($data);
+require_once __DIR__ . '/../includes/LunarMonthCalculator.php';
+$lmc = new LunarMonthCalculator();
+print_r($lmc->getMonthForDate(2026, 8, 6, 'Krishna'));
