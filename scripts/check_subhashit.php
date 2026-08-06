226318af14596a3148b5491ba24ff3a6550e82e9
Mayurrrrrrrrrr <mayurdubey2003@gmail.com>
2026-08-06 19:07:37 +0530

    Test clear cache

--- scripts/check_subhashit.php
+++ scripts/check_subhashit.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/../config/db.php';
-require_once __DIR__ . '/../includes/LunarMonthCalculator.php';
-$lmc = new LunarMonthCalculator();
-print_r($lmc->getMonthForDate(2026, 8, 6, 'Krishna'));
+require_once __DIR__ . '/../includes/PanchangHelper.php';
+$pdo->query("DELETE FROM panchang_data WHERE panchang_date = '2026-08-06'");
+$data = PanchangHelper::getForDate($pdo, '2026-08-06', 1);
+print_r($data);
