5916470764c4eae3bd26188114e1834cd95642e4
Mayurrrrrrrrrr <mayurdubey2003@gmail.com>
2026-08-07 07:28:06 +0530

    Check both

--- check.php
+++ check.php
@@ -1,4 +1,11 @@
 <?php
 require 'config/db.php';
 require 'includes/PanchangHelper.php';
+require 'includes/LunarMonthCalculator.php';
+
+$lmc = new LunarMonthCalculator();
+echo "LMC Output:\n";
+print_r($lmc->getMonthForDate(2026, 8, 7, 'Krishna'));
+
+echo "\nPH Output:\n";
 print_r(PanchangHelper::getForDate($pdo, '2026-08-07'));
