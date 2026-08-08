d28fd03b73e1413a8d50bc168693fc6b9b14c47e
Mayurrrrrrrrrr <mayurdubey2003@gmail.com>
2026-08-08 21:46:33 +0530

    Clean up drop_tables.php

--- drop_tables.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once 'config/db.php';
-$pdo->exec("DROP TABLE IF EXISTS em_rooms, em_room_allotments, em_meals, em_meal_tracking, em_work_categories, em_work_assignments, em_schedule;");
-echo "Dropped tables successfully.\n";
